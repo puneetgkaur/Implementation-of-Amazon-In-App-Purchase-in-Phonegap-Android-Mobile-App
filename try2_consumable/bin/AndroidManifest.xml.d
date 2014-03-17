fd3cca0199bb7ca3a9a2a98b5ed7db4654c25d74
Puneet Kaur <puneet.gkaur@gmail.com>
2014-03-18 00:11:19 +0530

    removed try2_consumable

--- try2_consumable/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/mps/try2_consumable/bin/AndroidManifest.xml : \
-/home/mps/try2_consumable/AndroidManifest.xml \
--- try2_consumable/bin/example-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/home/mps/try2_consumable/bin/example-debug-unaligned.apk : \
-/home/mps/try2_consumable/bin/example.ap_ \
-/home/mps/try2_consumable/bin/classes.dex \
-/home/mps/try2_consumable/src/com/smartmobilesoftware/inappbilling/CordovaAbstractBillingObserver.jav \
-/home/mps/try2_consumable/libs/cordova-2.8.1.jar \
-/home/mps/try2_consumable/libs/in-app-purchasing-1.0.3.jar \
